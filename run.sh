002864fef24165a9bfdeeff9ac7d0b66726953aa
Sam Der <sder@uci.edu>
2024-11-16 16:06:19 -0500

    Fix syntax error

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash/
+#!/usr/bin/bash
 
-$HOME_DIR="/home/fayzah/scapy-rtps-experimentation"
+HOME_DIR="/home/fayzah/scapy-rtps-experimentation"
 sudo zmap -p 7400 -r 1 0.0.0.0/0 | export $(cat $HOME_DIR/.env) && python3 $HOME_DIR/amplification_vulnerability.py
