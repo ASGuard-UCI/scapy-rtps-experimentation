b71536ecae81f112cd4c5637674120421e327526
Sam Der <sder@uci.edu>
2024-11-16 16:03:02 -0500

    Convert to bash script and use sudo

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-#!/bin/sh/
+#!/bin/bash/
+
 $HOME_DIR="/home/fayzah/scapy-rtps-experimentation"
-zmap -p 7400 -r 1 0.0.0.0/0 | export $(cat $HOME_DIR/.env) && python3 $HOME_DIR/amplification_vulnerability.py
+sudo zmap -p 7400 -r 1 0.0.0.0/0 | export $(cat $HOME_DIR/.env) && python3 $HOME_DIR/amplification_vulnerability.py
