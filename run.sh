64fc44baf1418be5df3e05373270341cbd19ce03
Sam Der <sder@uci.edu>
2024-11-16 16:30:08 -0500

    Make only root user able to run script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 HOME_DIR="/home/fayzah/scapy-rtps-experimentation"
-sudo zmap -p 11311 -r 1 0.0.0.0/0 | export $(cat $HOME_DIR/.env) && $HOME_DIR/.venv/bin/python3 $HOME_DIR/amplification_vulnerability.py
+zmap -p 11311 -r 1 0.0.0.0/0 | export $(cat $HOME_DIR/.env) && $HOME_DIR/.venv/bin/python3 $HOME_DIR/amplification_vulnerability.py
