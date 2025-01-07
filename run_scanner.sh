7b5fc94bd998edc8c19a455e3f6a99ca727bae71
Sam Der <sder@uci.edu>
2025-01-06 22:18:29 -0500

    Remove rate limit and call to now unused .env file

--- run_scanner.sh
+++ run_scanner.sh
@@ -2,5 +2,4 @@
 
 HOME_DIR="/home/fayzah/scapy-rtps-experimentation"
 
-export $(cat $HOME_DIR/.env)
-zmap -p 11311 -r 1 0.0.0.0/0 | $HOME_DIR/.venv/bin/python3 $HOME_DIR/amplification_vulnerability.py
+zmap -p 11311 0.0.0.0/0 | $HOME_DIR/.venv/bin/python3 $HOME_DIR/amplification_vulnerability.py
