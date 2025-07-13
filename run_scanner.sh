d692945ef93370569b33bb4472066026945096e9
Sam Der <samder713@gmail.com>
2025-07-13 14:17:27 -0700

    Remove -p option based on ZMap fork update
    See 7d1b436d4f69172c82e8479b09e863fe17ffeb24

--- run_scanner.sh
+++ run_scanner.sh
@@ -2,4 +2,6 @@
 
 HOME_DIR="$HOME/scapy-rtps-experimentation"
 
-zmap -p 11311 0.0.0.0/0 | $HOME_DIR/.venv/bin/python3 $HOME_DIR/amplification_vulnerability.py
+# This script requires usage of our ZMap fork located at ASGuard-UCI/zmap
+# instead of the original ZMap.
+zmap 0.0.0.0/0 | $HOME_DIR/.venv/bin/python3 $HOME_DIR/amplification_vulnerability.py
